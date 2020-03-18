59de9ac9fef3ed4db3c301614b2336d7b9796e7d
abdulrahmanafa <master197723@gmail.com>
2020-03-18 16:41:30 +0300

    Initial Commit

---
+++ nav&toolbar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nav&toolbar
+//
+//  Created by Abdulrhman A F  Alomairi on 3/18/20.
+//  Copyright © 2020 Abdulrhman A F  Alomairi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
